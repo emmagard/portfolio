2f990081f7290d0e327203c840a5bdae8f024811
Emma Gardner <emmagardner@outlook.com>
2014-11-24 21:48:47 -0500

    moved skills to be included in portfolio view. removed tooltip from main.js- Didn't need it.

--- instant/theme/assets/js/main.js
+++ instant/theme/assets/js/main.js
@@ -2,10 +2,8 @@ $(document).ready(function(){
   $('#portfolio-view').click(function(event){
     event.preventDefault();
     $('html, body').animate({
-        scrollTop: $('#works').offset().top -10
+        scrollTop: $('#skills').offset().top -10
     }, 1200);
   });
-
-  $('#email').tooltip();
 
 });
